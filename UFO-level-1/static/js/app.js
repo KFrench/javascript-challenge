98d43d5046da13f464d86a07ccadd87e59073682
Kaleah French <kaleah.french@gmail.com>
2020-03-21 12:42:29 -0400

    updates to app file

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,20 @@
 // from data.js
 var tableData = data;
 
+var tbody = d3.select("tbody");
 // YOUR CODE HERE!
+function buildtable(ufodata){
+ufodata.forEach(function(sitings) {
+  console.log(sitings);
+  var row = tbody.append("tr");
+
+  Object.entries(sitings).forEach(function([key, value]) {
+    console.log(key, value);
+    // Append a cell to the row for each value
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
+}
+buildtable(tableData);
+
